0fa855c92dca0b9176f86b867eaa3827b7a4cb57
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-12-10 22:14:39 +0600

    ticket successfully created

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors = require('cors')    
+const cors = require('cors')
 const app = express();
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -11,37 +11,59 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fcejyck.mongodb.net/?appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
 
+        const db = client.db('ticket_bari_DB');
+        const ticketsCollection = db.collection('tickets');
+        app.post('/tickets', async (req, res) => {
+            try {
+                const newTicket = req.body;
 
+                // Just insert
+                const result = await ticketsCollection.insertOne(newTicket);
 
-    // FTWgCKOuqsDWy9Af
-    // ticket_bari_user
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+                res.json({
+                    success: true,
+                    message: "Ticket created successfully!",
+                    ticketId: result.insertedId
+                });
+
+            } catch (error) {
+                res.status(500).json({
+                    success: false,
+                    message: "Something went wrong!",
+                    error: error.message
+                });
+            }
+        });
+
+        // FTWgCKOuqsDWy9Af
+        // ticket_bari_user
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Ticket Bari!')
+    res.send('Ticket Bari!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
